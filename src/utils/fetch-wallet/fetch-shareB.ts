94462dc1c87c90b01ca17b3caae672072bfce790
Kiet1618 <ngokiet3207@gmail.com>
2023-12-14 07:44:47 +0700

    fix

--- src/utils/fetch-wallet/fetch-shareB.ts
+++ src/utils/fetch-wallet/fetch-shareB.ts
@@ -17,33 +17,61 @@ import BN from 'bn.js';
 import { ec as EC } from 'elliptic';
 export const ec = new EC('secp256k1');
 
-const config = {
-  nodeEndpoints: [
-    'http://localhost:3001',
-    'http://localhost:3002',
-    'http://localhost:3003',
-  ],
-  nodePub: [
-    {
+
+const pingNode = async (endpoint: string) => {
+  try {
+    await axios.get(endpoint);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
+const configFetch = async () => {
+  const checkNode1: boolean = await pingNode('http://localhost:3001');
+  const checkNode2: boolean = await pingNode('http://localhost:3002');
+  const checkNode3: boolean = await pingNode('http://localhost:3003');
+  const nodeEndpoints = [];
+  const nodePub = [];
+  const indexes = [];
+  if (checkNode1) {
+    nodeEndpoints.push('http://localhost:3001');
+    nodePub.push({
       X: 'bc38813a6873e526087918507c78fc3a61624670ee851ecfb4f3bef55d027b5a',
       Y: 'ac4b21229f662a0aefdfdac21cf17c3261a392c74a8790db218b34e3e4c1d56a',
-    },
-    {
+    });
+    indexes.push(1);
+  }
+  if (checkNode2) {
+    nodeEndpoints.push('http://localhost:3002');
+    nodePub.push({
       X: 'b56541684ea5fa40c8337b7688d502f0e9e092098962ad344c34e94f06d293fb',
       Y: '759a998cef79d389082f9a75061a29190eec0cac99b8c25ddcf6b58569dad55c',
-    },
-    {
+    });
+    indexes.push(2);
+  }
+  if (checkNode3) {
+    nodeEndpoints.push('http://localhost:3003');
+    nodePub.push({
       X: '4b5f33d7dd84ea0b7a1eb9cdefe33dbcc6822933cfa419c0112e9cbe33e84b26',
       Y: '7a7813bf1cbc2ee2c6fba506fa5de2af1601a093d93716a78ecec0e3e49f3a57',
-    },
-  ],
-  indexes: [1, 2, 3],
-};
+    });
+    indexes.push(3);
+  }
+  if (nodeEndpoints.length < 2) throw new Error('Not enough nodes');
+
+  return {
+    nodeEndpoints,
+    nodePub,
+    indexes,
+  };
+}
 
 export const getAddress = async (
   input: GetAddressRequest,
 ): Promise<{ data?: GetAddressResponse; error?: ErrorApi }> => {
   let errorApi: ErrorApi;
+  const config = await configFetch();
   for (const endpoint of config.nodeEndpoints) {
     try {
       const { email } = input;
@@ -105,7 +133,7 @@ export const getPrivateKey = async (input: GetPrivateKeyRequest): Promise<{ data
     const pubKey = getPublic(tmpKey).toString("hex");
 
     const tokenCommitment = keccak256(idToken);
-    const { nodeEndpoints, indexes } = config;
+    const { nodeEndpoints, indexes } = await configFetch();
     const signatures = [];
 
     for (let i = 0; i < nodeEndpoints.length; i += 1) {
